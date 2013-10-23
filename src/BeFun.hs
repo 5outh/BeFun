06fdee3ee0d6d790ca8bbdbaa75500685232cea4
5outh <bkovach13@gmail.com>
2013-10-22 20:59:08 -0400

    Add a few basic binary functions

--- src/BeFun.hs
+++ src/BeFun.hs
@@ -1,37 +1,54 @@
 {-
-From http://en.wikipedia.org/wiki/Befunge#Befunge-93_instruction_list
-0-9	Push this number on the stack
-+	Addition: Pop a and b, then push a+b
--	Subtraction: Pop a and b, then push b-a
-*	Multiplication: Pop a and b, then push a*b
-/	Integer division: Pop a and b, then push b/a, rounded down. If a is zero, ask the user what result they want.[dubious � discuss]
-%	Modulo: Pop a and b, then push the remainder of the integer division of b/a. If a is zero, ask the user what result they want.[dubious � discuss]
-!	Logical NOT: Pop a value. If the value is zero, push 1; otherwise, push zero.
-`	Greater than: Pop a and b, then push 1 if b>a, otherwise zero.
->	Start moving right
-<	Start moving left
-^	Start moving up
-v	Start moving down
-?	Start moving in a random cardinal direction
-_	Pop a value; move right if value=0, left otherwise
-|	Pop a value; move down if value=0, up otherwise
-"	Start string mode: push each character's ASCII value all the way up to the next "
-:	Duplicate value on top of the stack
-\	Swap two values on top of the stack
-$	Pop value from the stack and discard it
-.	Pop value and output as an integer
-,	Pop value and output as ASCII character
-#	Trampoline: Skip next cell
-p	A "put" call (a way to store a value for later use). Pop y, x and v, then change the character at the position (x,y) in the program to the character with ASCII value v
-g	A "get" call (a way to retrieve data in storage). Pop y and x, then push ASCII value of the character at that position in the program
-&	Ask user for a number and push it
-~	Ask user for a character and push its ASCII value
-@	End program
- (space)	No-op. Does nothing
+  From http://en.wikipedia.org/wiki/Befunge#Befunge-93_instruction_list
+  0-9	Push this number on the stack
+  +	Addition: Pop a and b, then push a+b
+  -	Subtraction: Pop a and b, then push b-a
+  *	Multiplication: Pop a and b, then push a*b
+  /	Integer division: Pop a and b, then push b/a, rounded down. If a is zero, ask the user what result they want.[dubious � discuss]
+  %	Modulo: Pop a and b, then push the remainder of the integer division of b/a. If a is zero, ask the user what result they want.[dubious � discuss]
+  !	Logical NOT: Pop a value. If the value is zero, push 1; otherwise, push zero.
+  `	Greater than: Pop a and b, then push 1 if b>a, otherwise zero.
+  >	Start moving right
+  <	Start moving left
+  ^	Start moving up
+  v	Start moving down
+  ?	Start moving in a random cardinal direction
+  _	Pop a value; move right if value=0, left otherwise
+  |	Pop a value; move down if value=0, up otherwise
+  "	Start string mode: push each character's ASCII value all the way up to the next "
+  :	Duplicate value on top of the stack
+  \	Swap two values on top of the stack
+  $	Pop value from the stack and discard it
+  .	Pop value and output as an integer
+  ,	Pop value and output as ASCII character
+  #	Trampoline: Skip next cell
+  p	A "put" call (a way to store a value for later use). Pop y, x and v, then change the character at the position (x,y) in the program to the character with ASCII value v
+  g	A "get" call (a way to retrieve data in storage). Pop y and x, then push ASCII value of the character at that position in the program
+  &	Ask user for a number and push it
+  ~	Ask user for a character and push its ASCII value
+  @	End program
+   (space)	No-op. Does nothing
 -}
 
 -- TESTING HOME COMPUTER / SOURCETREE
 
 import Types
 import Data.Monoid
+import Control.Monad.State
+
+{- data BefungeState = BefungeState
+  {instructions :: Torus BefungeOperation, 
+   stack :: [Int],
+   arrayLoc :: Point, 
+   dir :: Direction} deriving Show -}
    
+num a (BefungeState is xs loc dir)= Right $ BefungeState is (a:xs) loc dir
+
+bsPopFunBinary f (BefungeState is (x:y:xs) loc dir) = Right $ BefungeState is ((f x y):xs) loc dir
+bsPopFunBinary f (BefungeState is xs loc dir) | length xs < 2 = Left "Attempt to perform binary operation without enough numbers in stack"
+
+add     = bsPopFunBinary (+)
+subt    = bsPopFunBinary (-)
+mult    = bsPopFunBinary (*)
+divide  = bsPopFunBinary (\a b -> if a == 0 then 0 else b `div` a)
+modulo  = bsPopFunBinary (\a b -> if a == 0 then 0 else b `mod` a)
